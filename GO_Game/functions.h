b9f7551a650c942db0b79ff7a79c7cfb492c8933
gshirato <gota.shirato@eleves.ec-nantes.fr>
2016-12-08 12:17:53 +0100

    Create functions.h

---
+++ GO_Game/functions.h
@@ -0,0 +1,13 @@
+/* 
+ * File:   functions.h
+ * Author: user
+ *
+ * Created on 8 décembre 2016, 09:08
+ */
+
+#ifndef FUNCTIONS_H
+#define	FUNCTIONS_H
+
+void Kaikyoku(goban g, int n);
+#endif	/* FUNCTIONS_H */
+
