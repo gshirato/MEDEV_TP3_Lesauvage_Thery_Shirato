5cb3a4d24aedf115e21d417b6f56413750d0e9eb
user <user@user-VirtualBox>
2016-12-08 08:47:41 +0100

    Creation du main

---
+++ GO_Game/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: user
+ *
+ * Created on 8 décembre 2016, 08:46
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
