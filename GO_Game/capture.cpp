ba34202551bc51bf9654f8a3dd0a4c046ee22a9d
user <user@user-VirtualBox>
2016-12-08 10:41:27 +0100

    début des fonctions test_capture et touche_diff

--- GO_Game/capture.cpp
+++ GO_Game/capture.cpp
@@ -1,1 +1,30 @@
+#include "constantes.h"
+#include "capture.h"
+#include <cstdlib>
+#include <iostream>
+#include <vector>
 
+//on créé une fonction qui va prendre en argument le tableau et la pièce qu'on
+//veut mettre et renvoie true ou false sans changer le tableau, on peut ainsi
+//utiliser cette fonction pour voir si on est dans un cas de suicide ou pas
+
+bool test_capture(char tableau[MSIZE][MSIZE],int i, int j, char couleur){
+    bool capture = false;
+    
+    
+    
+    
+    
+    return capture;
+}
+
+
+//fonction pour voir si un pion touche un ou plusieurs autres pions
+//renvoie un vect<int> avec, pour chaque pion adj de couleur diff i puis j
+//prend en arg le tableau 
+std::vector<int> touche_diff(char tableau[MSIZE][MSIZE],int i,int j,char couleur){
+    std::vector<int> touches;
+    
+    
+    return touches;
+}
