ca4528538faed7d8dd32c0c41899486c43cd28a8
user <user@user-VirtualBox>
2016-12-08 10:39:19 +0100

    fichier pour avoir les constantes, pour l'instant, uniquement la taille du tableau

---
+++ GO_Game/constantes.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   constantes.h
+ * Author: user
+ *
+ * Created on 8 décembre 2016, 10:16
+ */
+
+#ifndef CONSTANTES_H
+#define	CONSTANTES_H
+
+const int MSIZE = 5;
+
+#endif	/* CONSTANTES_H */
+
