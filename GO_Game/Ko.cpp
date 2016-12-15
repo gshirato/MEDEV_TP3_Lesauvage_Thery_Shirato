2689185f8444a7b456a4b2f04a79490b22b87afa
gshirato <gota.shirato@eleves.ec-nantes.fr>
2016-12-15 12:28:03 +0100

    Create Ko.cpp

---
+++ GO_Game/Ko.cpp
@@ -0,0 +1,16 @@
+#include "Go.h"
+#include "Ko.h"
+
+// Pas terminée
+void IsKo(int type, int y, int x, goban g){
+    int enleveUp;
+    int enleveRi;
+    int enleveDw;
+    int enleveLe;
+    int enleveTotal;
+    int koflag;
+    
+    if(type != g.board[y - 1][x]&&type != board[y][x + 1]&&type = board[y + 1][x]&&type = board[y][x - 1]){
+        
+    }
+}
