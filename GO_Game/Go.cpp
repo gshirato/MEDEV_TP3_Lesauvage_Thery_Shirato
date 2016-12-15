072e25c2a1074da6d7bfe18fea091f848dbc9ae9
gshirato <gota.shirato@eleves.ec-nantes.fr>
2016-12-15 12:27:04 +0100

    Update Go.cpp

--- GO_Game/Go.cpp
+++ GO_Game/Go.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 goban::goban(int n) {
+    turn = 1;
     for (int _i = -1; _i < n + 1; _i++) {
         for (int _j = -1; _j < n + 1; _j++) {
             board[_i][_j] = OUT;
@@ -15,12 +16,13 @@ goban::goban(int n) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             board[i][j] = VIDE;
-            ren[i][j] = -1;
+            //            ren[i][j] = -1;
         }
     }
 }
 
 goban::goban() {
+    turn = 1;
     for (int _i = -1; _i < 6; _i++) {
         for (int _j = -1; _j < 6; _j++) {
             board[_i][_j] = OUT;
@@ -29,7 +31,7 @@ goban::goban() {
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
             board[i][j] = VIDE;
-            ren[i][j] = -1;
+            //        ren[i][j] = -1;
         }
     }
 }
@@ -56,21 +58,21 @@ void goban::pierre(int type, int y, int x) {
     while (i == 0) {
         char char_x;
         int pos_x, pos_y;
-        
-       /* int around[4], raround[4];
-        around[0] = board[y - 1][x]; //    1
-        around[1] = board[y][x + 1]; //  4 N 2
-        around[2] = board[y + 1][x]; //    3
-        around[3] = board[y][x - 1]; //
 
-        raround[0] = ren[y - 1][x]; //    1
-        raround[1] = ren[y][x + 1]; //  4 N 2
-        raround[2] = ren[y + 1][x]; //    3
-        raround[3] = ren[y][x - 1]; //
+        /* int around[4], raround[4];
+         around[0] = board[y - 1][x]; //    1
+         around[1] = board[y][x + 1]; //  4 N 2
+         around[2] = board[y + 1][x]; //    3
+         around[3] = board[y][x - 1]; //
 
-        if (board[y][x] == VIDE) {
+         raround[0] = ren[y - 1][x]; //    1
+         raround[1] = ren[y][x + 1]; //  4 N 2
+         raround[2] = ren[y + 1][x]; //    3
+         raround[3] = ren[y][x - 1]; //
+         */
+        if (checklegal(type, y, x)) {
             board[y][x] = type;
-            for (int _i = 0; _i < 4; _i++) {
+            /*for (int _i = 0; _i < 4; _i++) {
                 if (around[_i] == type && raround[_i] == -1) {
                     ren[y][x] = counter;
                     raround[_i] = counter;
@@ -83,17 +85,13 @@ void goban::pierre(int type, int y, int x) {
             ren[y + 1][x] = raround[2];
             ren[y][x - 1] = raround[3];
             counter++;
-        * 
-        */
+             * 
+             */
             i++;
             break;
-        } else if (board[y][x] == BLANC || board[y][x] == NOIR) {
-
-            cout << "It's taken !! Reselect !" << endl;
-
         } else {
 
-            cout << "Invalid selection !! Reselect !" << endl;
+            cout << "Cannot put here!Reselect !" << endl;
 
         }
         cout << "Input Position X ex) B " << endl;
@@ -105,4 +103,24 @@ void goban::pierre(int type, int y, int x) {
         y = pos_y;
         x = pos_x;
     }
+}
+
+bool goban::checklegal(int type, int y, int x) {
+
+    if (board[y][x] != VIDE)
+        return false;
+    if (turn > 1) {
+        if (ko_x == x && ko_y == y && ko_num == turn - 1) {
+            return false;
+        }
+
+        if (CheckSuicide(type, y, x))
+            return false;
+
+        return true;
+    }
+}
+
+bool goban::CheckSuicide(int type, int y, int x) {
+    return false;
 }
